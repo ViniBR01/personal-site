a3a4659d3bffd9a6a82a45ade60bd4150729903b
ViniBR01 <vinicius@rice.edu>
2020-12-22 21:30:55 -0600

    modify siteRouting for clarity and organization

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,73 @@
 import React from 'react';
 import './App.css';
-import SiteRouter from '../Router/Router';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import NavBar from '../NavBar/NavBar';
 
-function App() {
-  return (
-    <SiteRouter />
-  );
+class App extends React.Component {
+    /* Initialize the state */
+    constructor(props) {
+        super(props);
+        this.handleResize = this.handleResize.bind(this);
+        this.state = {
+            height: window.innerHeight,
+            width: window.innerWidth
+        };
+    }
+
+    /* Function to maintain the window size updated */
+    handleResize() {
+        this.setState({
+            height: window.innerHeight,
+            width: window.innerWidth
+        });
+    }
+    /* Create event listener to check window resize */
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize);
+    }
+    /* Remove event listener when the componenet unmount */
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize);
+    }
+    
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <NavBar windowWidth={this.state.width} />
+                    <Switch>                        
+                        <Route exact path="/">
+                            Home page!
+                            <h1>Hello, world!</h1>
+                            <h3>height: {this.state.height}px</h3>
+                            <h3>width: {this.state.width}px</h3>
+                        </Route>
+                        <Route exact path="/about">
+                            About me!
+                        </Route>
+                        <Route exact path="/projects">
+                            Projects!
+                        </Route>
+                        <Route exact path="/photos">
+                            Photography!
+                        </Route>
+                        <Route exact path="/blog">
+                            My Blog!
+                        </Route>
+                        <Route exact path="/contact">
+                            Contact me!
+                        </Route>
+                        <Route path="*">
+                            404 Not Found!
+                        </Route>
+                    </Switch>
+                    <div>
+                        Footer content
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,139 +2,116 @@ import React from 'react';
 import './NavBar.css';
 import { NavLink } from 'react-router-dom';
 
-function logmessage(e) {
-    e.preventDefault();
-    console.log('The link was clicked.');
-}
-
 class NavBar extends React.Component {
+    /* Initialize the state */
+    constructor(props) {
+        super(props);
+        this.clickHamburguer = this.clickHamburguer.bind(this);
+        this.state = {
+            hamburguerButton: false
+        };
+    }
 
-
+    clickHamburguer(e) {
+        e.preventDefault();
+        console.log('The link was clicked.');
+    }
     
     render() {
         //console.log(this.props.windowDimension);
-        const isMobile = this.props.windowDimension <= 810;
+        const isMobile = this.props.windowWidth <= 810;
         let NavBarContent;
         if (isMobile) {
             NavBarContent = (
-
-
-                <div className="topbar-wrapper">
-                    <div className="topbar-box">
-                        <div className="navbar-wrapper">
-                            <div className="navbar-box">
-                                
-                                <div className="logo-wrapper">
-                                    <div className="logo-box">
-                                        <p className="logo">Mobile bar</p>
-                                    </div>
+                <div className="topbar-box">
+                    <div className="navbar-wrapper">
+                        <div className="navbar-box">
+                            <div className="logo-wrapper">
+                                <div className="logo-box">
+                                    <p className="logo">Mobile bar</p>
                                 </div>
-                                
-                                <div className="navitemsmobile-wrapper">
-                                    
-                                    <button className="navitemsmobile-button" type="button" onClick={logmessage}></button>
-                                    
-                                    
-                                </div>
-                                
+                            </div>
+                            <div className="navitemsmobile-wrapper">
+                                <button className="navitemsmobile-button" type="button" onClick={this.clickHamburguer}></button> 
                             </div>
                         </div>
-
-
-
-
-
-
-
-                        <nav className="navitemsmobile-box">
-                            
-                            <ul>
-                                <li>
-                                    <NavLink exact className="navmobile-link" to="/">Hello</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink exact className="navmobile-link" to="/about">About me</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink exact className="navmobile-link" to="/projects">Projects</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink exact className="navmobile-link" to="/photos">Photos</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink exact className="navmobile-link" to="/blog">Blog</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink exact className="navmobile-link" to="/contact">Contact</NavLink>
-                                </li>
-                            </ul>
-                            
-                        </nav>
-
-
-
-
-
-
-
-                        
-
-
-                        
                     </div>
+                    <nav className="navitemsmobile-box">
+                        <ul>
+                            <li>
+                                <NavLink exact className="navmobile-link" to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink exact className="navmobile-link" to="/about">About me</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink exact className="navmobile-link" to="/projects">Projects</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink exact className="navmobile-link" to="/photos">Photos</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink exact className="navmobile-link" to="/blog">Blog</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink exact className="navmobile-link" to="/contact">Contact</NavLink>
+                            </li>
+                        </ul>
+                        
+                    </nav>        
                 </div>
+                
             );
         } else {
             NavBarContent = (
-                <div className="topbar-wrapper">
-                    <div className="topbar-box">
-                        <div className="navbar-wrapper">
-                            <div className="navbar-box">
-                                
-                                <div className="logo-wrapper">
-                                    <div className="logo-box">
-                                        <p className="logo">Vini Da Silva</p>
-                                    </div>
+                <div className="topbar-box">
+                    <div className="navbar-wrapper">
+                        <div className="navbar-box">
+                            
+                            <div className="logo-wrapper">
+                                <div className="logo-box">
+                                    <p className="logo">Vini Da Silva</p>
                                 </div>
-                                
-                                <div className="navitems-wrapper">
-                                    <nav className="navitems-box">
-                                        
-                                        <ul className="navitems-list">
-                                            <li className="nav-item">
-                                                <NavLink exact className="nav-link" to="/">Hello</NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink exact className="nav-link" to="/about">About me</NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink exact className="nav-link" to="/projects">Projects</NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink exact className="nav-link" to="/photos">Photos</NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink exact className="nav-link" to="/blog">Blog</NavLink>
-                                            </li>
-                                            <li className="nav-item">
-                                                <NavLink exact className="nav-link" to="/contact">Contact</NavLink>
-                                            </li>
-                                        </ul>
-                                        
-                                    </nav>
-                                </div>
+                            </div>
+                            
+                            <div className="navitems-wrapper">
+                                <nav className="navitems-box">
+                                    
+                                    <ul className="navitems-list">
+                                        <li className="nav-item">
+                                            <NavLink exact className="nav-link" to="/">Home</NavLink>
+                                        </li>
+                                        <li className="nav-item">
+                                            <NavLink exact className="nav-link" to="/about">About me</NavLink>
+                                        </li>
+                                        <li className="nav-item">
+                                            <NavLink exact className="nav-link" to="/projects">Projects</NavLink>
+                                        </li>
+                                        <li className="nav-item">
+                                            <NavLink exact className="nav-link" to="/photos">Photos</NavLink>
+                                        </li>
+                                        <li className="nav-item">
+                                            <NavLink exact className="nav-link" to="/blog">Blog</NavLink>
+                                        </li>
+                                        <li className="nav-item">
+                                            <NavLink exact className="nav-link" to="/contact">Contact</NavLink>
+                                        </li>
+                                    </ul>
+                                    
+                                </nav>
                             </div>
                         </div>
                     </div>
                 </div>
+                
             );
         }
         
         return (
-            <div>
+            <div className="topbar-wrapper">
                 { NavBarContent }
             </div>
-                    
+            
         );
     }
 }
