a62a031dbcf46c6eecccc0e348e4a946bf2be717
ViniBR01 <vinicius@rice.edu>
2020-12-26 12:14:29 -0600

    Add contact page and file structure

---
+++ src/pages/contact/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './contact.css';
+import Title from '../../components/Title/Title';
+
+function Contact() {
+    return (
+        <div className="page-wrapper-contact">
+            <Title title_text="Don't be a stranger" />
+
+            
+        </div>
+    );
+}
+
+export default Contact;
