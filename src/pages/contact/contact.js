c0f9e48655baadd4f04327953f79e868760f7d12
ViniBR01 <vinicius@rice.edu>
2020-12-26 21:12:41 -0600

    Add form to contact me page and adjust css

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './contact.css';
 import Title from '../../components/Title/Title';
+import ContactForm from './ContactForm';
 import {
     FaFacebook,
     FaGithub,
@@ -14,43 +15,72 @@ function Contact() {
             <Title title_text="Don't be a stranger" />
 
             <div className="social-media-wrap" >
-                
-                <div className="social-media-title" ><h3>Social Media</h3></div>
-                
-                <div className="social-media-box" >
-                    <div className="social-media-icon">
-                        <FaLinkedin />
+                <div className="social-media-container">
+                    
+                    <div className="social-media-title" >
+                        <h2>Social Media</h2>
+                        <p>Follow me on my social media and let's talk</p>
                     </div>
-                    <p>@vini-goncalves</p>
-                </div>
 
-                <div className="social-media-box" >
-                    <div className="social-media-icon">
-                        <FaGithub />
-                    </div>
-                    <p>@ViniBR</p>
-                </div>
-
-                <div className="social-media-box" >
-                    <div className="social-media-icon">
-                        <FaFacebook />
-                    </div>
-                    <p>@VGoncalves29</p>
-                </div>
-
-                <div className="social-media-box" >
-                    <div className="social-media-icon">
-                        <FaYoutube />
-                    </div>
-                    <p>@channel</p>
+                    <a className='contact-socialmedia-link'
+                       target="_blank"
+                       rel="noopener noreferrer"
+                       href="https://www.linkedin.com/in/vini-goncalves/"
+                    >
+                        <div className="social-media-box" >
+                            <div className="social-media-icon">
+                                <FaLinkedin />
+                            </div>
+                            <p>@vini-goncalves</p>
+                        </div>
+                    </a>
+                    <a className='contact-socialmedia-link'
+                       target="_blank"
+                       rel="noopener noreferrer"
+                       href="https://github.com/ViniBR01"
+                    >
+                        <div className="social-media-box" >
+                            <div className="social-media-icon">
+                                <FaGithub />
+                            </div>
+                            <p>@ViniBR</p>
+                        </div>
+                    </a>
+                    <a className='contact-socialmedia-link'
+                       target="_blank"
+                       rel="noopener noreferrer"
+                       href="https://www.facebook.com/vgoncalves29"
+                    >
+                        <div className="social-media-box" >
+                            <div className="social-media-icon">
+                                <FaFacebook />
+                            </div>
+                            <p>@VGoncalves29</p>
+                        </div>
+                    </a>
+                    <a className='contact-socialmedia-link'
+                       target="_blank"
+                       rel="noopener noreferrer"
+                       href="https://www.youtube.com/channel/UCuWu3CcWPARJzqw3v7mXUxw"
+                    >
+                        <div className="social-media-box" >
+                            <div className="social-media-icon">
+                                <FaYoutube />
+                            </div>
+                            <p>@channel</p>
+                        </div>
+                    </a>
                 </div>
             </div>
             
             <div className="message-form-wrap" >
-                <div>
-                    <div><h3>Message Me</h3></div>
+                <div className="message-form-container">
                     <div>
-                        Name-email-message
+                        <h2>Message me</h2>
+                        <p>Or send me an e-mail using the form below</p>
+                    </div>
+                    <div>
+                        <ContactForm />
                     </div>
                 </div>
             </div>
