2a570281840ffa7147781fe78a6b2f50f8fcb8c1
ViniBR01 <vinicius@rice.edu>
2020-12-26 22:25:09 -0600

    Add send email functionality with emailJS

--- src/pages/contact/ContactForm.js
+++ src/pages/contact/ContactForm.js
@@ -2,46 +2,70 @@ import React from 'react';
 import './ContactForm.css';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
- 
- const ContactForm = () => {
-     return (
-         <div className="form-wrapper" >
-             <Formik
-                 initialValues={{ name: '', email: '', message: '' }} 
-                 validationSchema={Yup.object({
-                     name: Yup.string()
-                              .max(30, 'Must be 30 characters or less')
-                              .required('*Required'),
-                     message: Yup.string()
-                                 .max(2000, '*Must be 2000 characters or less')
-                                 .required('*Required'),
-                     email: Yup.string().email('*Invalid email address').required('*Required') 
-                 })} 
-                 onSubmit={(values, { setSubmitting }) => {
-                     setTimeout(() => {
-                         alert(JSON.stringify(values, null, 2));
-                         setSubmitting(false);
-                     }, 400);
-                 }}
-         >
-         <Form>
-             <label htmlFor="name">Name</label>
-             <Field name="name" type="text" />
-             <ErrorMessage name="name" render={msg => <div className="form-error-message">{msg}</div>} />
-             
-             <label htmlFor="email">E-mail Address</label>
-             <Field name="email" type="email" />
-             <ErrorMessage name="email" render={msg => <div className="form-error-message">{msg}</div>} />
+import emailjs from 'emailjs-com';
 
-             <label htmlFor="message">Your Message</label>
-             <Field as="textarea" name="message" type="text" />
-             <ErrorMessage name="message" render={msg => <div className="form-error-message">{msg}</div>} />
-             
-             <button type="submit">Send Message</button>
-         </Form>
-             </Formik>
-         </div>
-     );
- };
+
+
+const ContactForm = () => {
+
+    
+    return (
+        <div className="form-wrapper" >
+            <Formik
+                initialValues={{ name: '', email: '', message: '' }} 
+                validationSchema={Yup.object({
+                    name: Yup.string()
+                             .max(30, 'Must be 30 characters or less')
+                             .required('*Required'),
+                    message: Yup.string()
+                                .max(2000, '*Must be 2000 characters or less')
+                                .required('*Required'),
+                    email: Yup.string().email('*Invalid email address').required('*Required') 
+                })} 
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        setSubmitting(false);
+
+                        emailjs.send(
+                            'gmail-vini',
+                            'template_lkyvrex',
+                            {
+                                from_name: values.name,
+                                from_email: values.email,
+                                message: values.message
+                            },
+                            'user_UyG8S38eIy1c8wlQTNpOI')
+                               .then(function(response) {
+                                   console.log('SUCCESS!', response.status, response.text);
+                                   alert("Thank you for your message! I'll reply ASAP.");
+                               }, function(error) {
+                                   console.log('FAILED...', error);
+                                   alert("Something went wrong. Please try again or contat me at vinicius@rice.edu.");
+                               });
+                        
+                    }, 400);
+                    
+                    
+                }}
+            >
+                <Form>
+                    <label htmlFor="name">Name</label>
+                    <Field name="name" type="text" />
+                    <ErrorMessage name="name" render={msg => <div className="form-error-message">{msg}</div>} />
+                    
+                    <label htmlFor="email">E-mail Address</label>
+                    <Field name="email" type="email" />
+                    <ErrorMessage name="email" render={msg => <div className="form-error-message">{msg}</div>} />
+
+                    <label htmlFor="message">Your Message</label>
+                    <Field as="textarea" name="message" type="text" />
+                    <ErrorMessage name="message" render={msg => <div className="form-error-message">{msg}</div>} />
+                    
+                    <button type="submit">Send Message</button>
+                </Form>
+            </Formik>
+        </div>
+    );
+};
 
 export default ContactForm;
