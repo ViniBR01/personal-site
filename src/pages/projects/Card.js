79a74297fc86ca30252fa910d681bfadb3bafda3
ViniBR01 <vinicius@rice.edu>
2020-12-28 18:04:49 -0600

    improve style of card to accept various image sizes

--- src/pages/projects/Card.js
+++ src/pages/projects/Card.js
@@ -4,29 +4,32 @@ import './Card.css';
 function Card({ title, avatar, text, badges, link }) {
     return (
         <div className="card-wrapper">
-        <div className="card-container" >
-        <div className="card-img-container" >
-        {
-            link && link.length > 0 ?
-                                  <a href={ link } target="_blank" rel="noopener noreferrer">
-                                      <img className="card-img" src={ avatar } alt=""/>
-                                  </a>
-:
-                                  <img className="card-img" src={ avatar } alt=""/>
-        }
-        </div>
-        <div className="card-body-container" >
-        <h4 className="card-body-title" >{ title }</h4>
-        <div className="card-body-text" >{ text }</div>
-
-        <div className="card-body-badges-wrap" >
-            {
-                badges && badges.map((badge, index) =>
-                    <div className="card-body-badges" key={ index }>{ badge }</div>)
-            }
-        </div>
-        </div>
-        </div>
+            <div className="card-container" >
+                <div className="card-img-wrapper">
+                    <div className="card-img-container" >
+                        {
+                            link && link.length > 0 ?
+                                                  <a href={ link } target="_blank" rel="noopener noreferrer">
+                                                      <img className="card-img" src={ avatar } alt=""/>
+                                                  </a>
+                            :
+                                                  <img className="card-img" src={ avatar } alt=""/>
+                        }
+                    </div>
+                </div>
+                
+                <div className="card-body-container" >
+                    <h4 className="card-body-title" >{ title }</h4>
+                    <div className="card-body-text" >{ text }</div>
+                    
+                    <div className="card-body-badges-wrap" >
+                        {
+                            badges && badges.map((badge, index) =>
+                                <div className="card-body-badges" key={ index }>{ badge }</div>)
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
